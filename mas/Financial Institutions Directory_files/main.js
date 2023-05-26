97e342d6fe6b72728a8c72a3544b81157323e188
cjanning-sc <chris.janning@sitecore.com>
2023-05-26 01:06:03 -0500

    mas

---
+++ mas/Financial Institutions Directory_files/main.js
@@ -0,0 +1,12 @@
+// notification lib
+var myNotification;
+// ready
+$(function() {
+
+	// UI Interaction
+	var masUI = new MAS.UIComponents();
+	// notification lib
+	// myNotification = new Lib.PopupNotification();
+	// initialize ui component
+	masUI.init();
+});
